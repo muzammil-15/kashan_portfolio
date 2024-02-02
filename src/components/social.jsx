d8be4c86390fef4907b3a79256f042556addaa2b
muzammil-15 <muzammilcoder15@gmail.com>
2024-02-02 11:41:54 +0500

    changes in about section

--- src/components/social.jsx
+++ src/components/social.jsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 export default function Social({ href, image }) {
   return (
-    <section className="w-full relative  overflow-hidden h-[250px] sm:w-[80%] md:w-[40%] lg:w-[30%] rounded-lg bg-contain bg-center hover:shadow-[0_0px_30px_0px_rgba(167,_152,_152,_0.57)] hover:overflow-hidden">
+    <section className="w-full relative  overflow-hidden h-[250px] sm:w-[80%] md:w-[40%] lg:w-[30%] rounded-[18px] bg-contain bg-center hover:shadow-[0_0px_30px_0px_rgba(167,_152,_152,_0.57)] hover:overflow-hidden">
     <Link href={href} alt={"social images"} className="overflow-hidden " target="_blank">
         <Image src={image} fill/>
 
