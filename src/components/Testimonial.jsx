6c127a1247f296533f16e4b7a45f99d8347a79cf
muzammil-15 <muzammilcoder15@gmail.com>
2024-02-06 12:02:55 +0500

    changes in testimonial section

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,7 +1,13 @@
 import Image from "next/image";
-import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "./ui/carousel";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "./ui/carousel";
 import { testimonials } from "@/lib/utils";
-import Autoplay from "embla-carousel-autoplay"
+import Autoplay from "embla-carousel-autoplay";
 import AutoScroll from "embla-carousel-auto-scroll";
 
 const Testimonail = () => {
@@ -19,35 +25,58 @@ const Testimonail = () => {
             Testimonials
           </h2>
         </div>
-        <div className="relative my-10 w-full">
-          <Carousel className="md:mx-10" plugins={[
-        AutoScroll(
-          {
-            startDelay: 0,
-            stopOnInteraction: false,
-            stopOnFocusIn: false,
-          }
-        )
-      ]}>
+        <div className="relative md:my-10 w-full">
+          <Carousel
+            className="md:mx-10"
+            opts={{
+              align: "start",
+              loop: true,
+            }}
+            plugins={[
+              AutoScroll({
+                startDelay: 0,
+                stopOnInteraction: false,
+                stopOnFocusIn: false,
+              }),
+            ]}
+          >
             <CarouselContent className="">
-              {
-                testimonials.map((testi, ind)=>(
-                  <CarouselItem key={ind} className=" relative flex justify-center items-center p-10">
-                    <div className=" w-[80%] bg-[#1A1A1A] p-2 md:p-6 z-20">
-                      <div className="bg-[#525252] p-2 md:p-6 flex justify-center items-center flex-col gap-5">
-                        <p className="text-center w-full md:w-[90%] lg:w-[70%] text-[10px] md:text-base ">{testi.desc}</p>
-                        <pre>{testi.name}</pre>
-
-                      </div>
-                      
+              {testimonials.map((testi, ind) => (
+                <CarouselItem
+                  key={ind}
+                  className=" relative flex justify-center items-center p-10"
+                >
+                  <div className="w-full md:w-[80%] bg-[#1A1A1A] p-2 md:p-6 z-20">
+                    <div className="bg-[#525252] p-2 md:p-6 flex justify-center items-center flex-col gap-5">
+                      <p className="text-center w-full md:w-[90%] lg:w-[70%] text-[10px] md:text-base ">
+                        {testi.desc}
+                      </p>
+                      <pre>{testi.name}</pre>
                     </div>
-                    <Image src={'/assets/dots.png'} width={250} height={250} alt="dots" className="absolute top-[-50px] right-16 z-10 hidden md:block"/>
-                    <Image src={'/assets/dots.png'} width={250} height={250} alt="dots" className="absolute bottom-[-50px] left-20 z-10 hidden md:block"/>
-                    <Image src={testi.image} width={50} height={50} alt="client" className="absolute bottom-[0px] left-[50%] z-[999]"/>
-                  </CarouselItem>
-                ))
-              }
-              
+                  </div>
+                  <Image
+                    src={"/assets/dots.png"}
+                    width={250}
+                    height={250}
+                    alt="dots"
+                    className="absolute top-[-50px] right-16 z-10 hidden md:block"
+                  />
+                  <Image
+                    src={"/assets/dots.png"}
+                    width={250}
+                    height={250}
+                    alt="dots"
+                    className="absolute bottom-[-50px] left-20 z-10 hidden md:block"
+                  />
+                  <Image
+                    src={testi.image}
+                    width={70}
+                    height={70}
+                    alt="client"
+                    className="absolute bottom-[0px] right-0 sm:right-5 md:right-20 z-[999]"
+                  />
+                </CarouselItem>
+              ))}
             </CarouselContent>
             {/* <CarouselPrevious />
             <CarouselNext /> */}
